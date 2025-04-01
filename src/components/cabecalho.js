7f778df9a7c39fdb987ed94220b8cf6502c8a9b5
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2025-04-01 20:39:48 -0300

    responsividade do navbar e ajustes incrementais

--- src/components/cabecalho.js
+++ src/components/cabecalho.js
@@ -4,59 +4,58 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import "../components/cabecalho.css"; // Importa o CSS
 
 const NavbarFinancas = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <AppBar position="static">
-      <Toolbar className="cabecalho">
-        {/* Logo à esquerda */}
-        <Box>
-          <img
-            src="../images/logo-feasy-branco.svg"
-            alt="logo"
-            className="logo-esquerda"
-          />
-        </Box>
+    return (
+        <AppBar position="static">
+            <Toolbar className="cabecalho">
+                {/* Logo à esquerda */}
+                <Box>
+                    <img
+                        src="../images/logo-feasy-branco.svg"
+                        alt="logo"
+                        className="logo-esquerda"
+                    />
+                </Box>
 
-        {/* Título Central */}
-        <Box textAlign="center">
-          <img
-            src="../images/logo-feasy-branco.svg"
-            alt="logo"
-            className="logo-meio"
-          />
-          <h6 className="logo-meio">Finanças Pessoais</h6>
-        </Box>
+                {/* Título Central */}
+                <Box textAlign="center">
+                    <img
+                        src="../images/logo-feasy-branco.svg"
+                        alt="logo"
+                        className="logo-meio"
+                    />
+                    <h1>Finanças pessoais</h1>
+                </Box>
 
-        {/* Menu Perfil */}
-        <Box>
-          <IconButton onClick={handleMenu} className="perfil-icon">
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            anchorOrigin={{ vertical: "top", horizontal: "right" }}
-            transformOrigin={{ vertical: "top", horizontal: "right" }}
-          >
-            <MenuItem onClick={handleClose}>Configurações</MenuItem>
-            <MenuItem onClick={handleClose}>Perfil</MenuItem>
-            <MenuItem onClick={() => (window.location.href = "/logout")}>Sair</MenuItem>
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
+                {/* Menu Perfil */}
+                <Box>
+                    <IconButton onClick={handleMenu} className="perfil-icon">
+                        <AccountCircle className="perfil-icon-size" />
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                        transformOrigin={{ vertical: "top", horizontal: "right" }}>
+                        <MenuItem onClick={handleClose}>Configurações</MenuItem>
+                        <MenuItem onClick={handleClose}>Perfil</MenuItem>
+                        <MenuItem onClick={() => (window.location.href = "/logout")}>Sair</MenuItem>
+                    </Menu>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default NavbarFinancas;
