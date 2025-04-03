14d99f5b0fbadbfe8952f4e1f9c6b96149c6b218
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2025-04-03 19:31:38 -0300

    alteração da barra de saldo

--- src/components/saldoConta.js
+++ src/components/saldoConta.js
@@ -2,37 +2,38 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 
 const SaldoAtual = ({ saldo }) => {
-  return (
-    <Box
-      sx={{
-        backgroundColor: "#37a69200",
-        paddingX: "5%",
-        marginTop: "5%",
-      }}
-    >
-      {/* Label */}
-      <Box sx={{ backgroundColor: "#db4ac300", padding: "8px 0" }}>
-        <Typography variant="h6" color="textPrimary">
-          Saldo atual
-        </Typography>
-      </Box>
+    return (
+        <Box
+        sx={{
+            backgroundColor: "#d4a373",
+            marginTop: "5%",
+          }}>
+        
 
-      {/* Saldo */}
-      <Box 
-        sx={{ 
-          display: "flex", 
-          justifyContent: "space-between", 
-          backgroundColor: "#5d816500", 
-          padding: "8px 0" 
-        }}
-      >
-        <Typography variant="h5" color="textPrimary">R$</Typography>
-        <Typography variant="h5" color="textPrimary" textAlign="right">
-          {saldo}
-        </Typography>
-      </Box>
-    </Box>
-  );
+            {/* Label */}
+            <Box sx={{ backgroundColor: "#ffc8dd", padding: "8px 0" }}>
+                <Typography variant="h6" color="textPrimary">
+                    Saldo atual
+                </Typography>
+            </Box>
+
+            {/* Saldo */}
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    backgroundColor: "#a2d2ff",
+                    padding: "8px 0"
+                }}
+            >
+
+                <Typography variant="h5" color="textPrimary">R$</Typography>
+                <Typography variant="h5" color="textPrimary" textAlign="right">
+                    {saldo}
+                </Typography>
+            </Box>
+        </Box>
+    );
 };
 
 export default SaldoAtual;
