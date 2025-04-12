9a480de88a378bffd4304ab55fa78482db97b09b
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2025-04-11 21:27:52 -0300

    alteração do label

--- src/components/Forms/novoLancamento.js
+++ src/components/Forms/novoLancamento.js
@@ -73,7 +73,7 @@ const NovoLancamento = () => {
           }}
         >
           <Typography variant="h6" component="h2" sx={{ mb: 2 }}>
-            Novo Lançamento
+            Novo lançamento
           </Typography>
 
           <Typography variant="subtitle2">Tipo de Lançamento</Typography>
