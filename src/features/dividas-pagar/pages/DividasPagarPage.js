d7069771c3f16e68aa9e6d400c2665b49356084b
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2025-05-14 20:56:20 -0300

    dividas a pagar

--- src/features/dividas-pagar/pages/DividasPagarPage.js
+++ src/features/dividas-pagar/pages/DividasPagarPage.js
@@ -1,9 +1,13 @@
 import SaldoAtual from '../../../components/shared/saldo-atual/SaldoAtual';
 const DividasPagarPage = () => {
     return (
-        <SaldoAtual/>
+        <div>
+            <SaldoAtual />
+            <div>Dividas a pagar</div>
+        </div>
     );
-  };
-  
-  export default DividasPagarPage;
-  
+
+
+};
+
+export default DividasPagarPage;
