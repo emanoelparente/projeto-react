30cf662fcb3bf7cc0265f0b34885b9e981e2152c
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2025-05-17 22:56:40 -0300

    atualização do background

--- src/features/auth/components/AuthCard.js
+++ src/features/auth/components/AuthCard.js
@@ -2,39 +2,27 @@ import React from "react";
 import "./AuthCard.css";
 
 const AuthCard = ({ children, title, subtitle, buttonText, onSubmit }) => {
-    return (
-
-        <div
-            className="auth-card-background"
-            style={{
-                backgroundImage: `url(${process.env.PUBLIC_URL}/images/background-login-signup.svg)`,
-               
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-            }}
-        >
-            <div className="auth-container">
-                <div className="auth-left">
-                    <h1 className="brand">feasy</h1>
-                    <h2>Bem vindo de volta!</h2>
-                    <p>Faça login e comece a gerenciar seu dinheiro</p>
-                    <button className="btn-login">{buttonText}</button>
-                </div>
-                <div className="auth-right">
-                    <h2>{title}</h2>
-                    <div className="social-login">
-                        <button className="btn-social">f</button>
-                        <button className="btn-social">G</button>
-                    </div>
-                    <p>Ou registre com o e-mail</p>
-                    <form onSubmit={onSubmit}>
-                        {children}
-                    </form>
-                </div>
-            </div>
+  return (
+    <div className="auth-container">
+      <div className="auth-left">
+        <h1 className="brand">feasy</h1>
+        <h2>Bem vindo de volta!</h2>
+        <p>Faça login e comece a gerenciar seu dinheiro</p>
+        <button className="btn-login">{buttonText}</button>
+      </div>
+      <div className="auth-right">
+        <h2>{title}</h2>
+        <div className="social-login">
+          <button className="btn-social">f</button>
+          <button className="btn-social">G</button>
         </div>
-
-    );
+        <p>Ou registre com o e-mail</p>
+        <form onSubmit={onSubmit}>
+          {children}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthCard;
